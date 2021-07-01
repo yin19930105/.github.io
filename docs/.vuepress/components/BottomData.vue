484f4ceef86edaa2cfa6a398118f8e1b625f1a68
豪横 <1624857254@qq.com>
2021-07-01 14:00:17 +0800

    初始化

---
+++ docs/.vuepress/components/BottomData.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="FirstBottomPage">
+        <STRONG style="color: #42b983">本文档由河北织羽网络科技有限公司提供</STRONG>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+.FirstBottomPage{
+    border-top: 1px solid #eaecef;
+    padding-top: 30px;
+    margin-top: 40px;
+    text-align: center;
+}
+</style>
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,49 @@
+module.exports = {
+    title: '织羽首页', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
+    description: '织羽', // meta 中的描述文字，用于SEO
+    // 注入到当前页面的 HTML <head> 中的标签
+    head: [
+        ['link', { rel: 'icon', href: '/zhiyu.png' }],  //浏览器的标签栏的网页图标
+    ],
+    markdown: {
+        lineNumbers: true
+    },
+    serviceWorker: true,
+    themeConfig: {
+        logo: '/zhiyu.png',
+        lastUpdated: 'lastUpdate', // string | boolean
+        nav: [
+            { text: '首页', link: '/' },
+            {
+                text: '分类',
+                ariaLabel: '分类',
+                items: [
+                    { text: '文章', link: '/pages/folder1/test1.md' },
+                    { text: '琐碎', link: '/pages/folder2/test4.md' },
+                ]
+            },
+            { text: '功能演示', link: '/pages/folder1/test3.md' },
+            { text: 'Gitee', link: 'https://e.gitee.com/hebei-zhiyu-network/repos/hebei-zhiyu-network/zhi-yu-etb-cloud' },
+        ],
+        sidebar: {
+            '/pages/folder1/':[
+                {
+                    title: '测试菜单1',   // 必要的
+                    collapsable: false, // 可选的, 默认值是 true,
+                    sidebarDepth: 1,    // 可选的, 默认值是 1
+                    children: [
+                        ['test1.md', '子菜单1'],
+                        ['test3.md', '子菜单2']
+                    ]
+                },
+                {
+                    title: '测试菜单2',
+                    collapsable: false, // 可选的, 默认值是 true,
+                    children: [
+                        ['test2.md', '子菜单1']
+                    ]
+                }
+            ],
+        }
+    }
+}
